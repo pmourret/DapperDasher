3179a2d464e753bfead7c90e544261005fc87086
pmourret <pierre.mourret.cav@protonmail.com>
2023-03-07 07:42:12 +0100

    Bruh

--- DapperDasher/DapperDasher/MainWindow.cpp
+++ DapperDasher/DapperDasher/MainWindow.cpp
@@ -1,1 +1,23 @@
 ﻿#include "MainWindow.h"
+#include <cstdio>
+#include <raylib.h>
+
+int main(int argc, char* argv[])
+{
+    InitWindow(windowWidth,windowHeight,"DapperDasher");
+    SetTargetFPS(60);
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("Hello World", textPosX, textPosY,16, WHITE);
+        textPosY += textDirection;
+        if (textPosY > windowHeight || textPosY < 0)
+        {
+            textDirection = -textDirection ;
+        }
+        EndDrawing();
+    }
+    CloseWindow();
+}
+
--- DapperDasher/DapperDasher/MainWindow.h
+++ DapperDasher/DapperDasher/MainWindow.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 
-struct MainWindow
-{
-    
-};
+const int windowHeight = 600 ;
+const int windowWidth = 800 ;
+
+int textPosX = 200;
+int textPosY = 200;
+int textDirection = 10 ;
